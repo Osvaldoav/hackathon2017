0abbd25172f3c09c5faa7a97c1b24d72991d8688
Mariano UG <marianok300@gmail.com>
2017-08-26 15:37:09 -0500

    modified home and indexjs

--- Vue/src/components/Home.vue
+++ Vue/src/components/Home.vue
@@ -11,6 +11,15 @@
     </v-layout>
   </v-container>
 -->
+<v-container>
+  <v-layout row>
+    <v-flex xs12>
+      <v-btn @click.prevent='onClickPolys'>
+        Create PolyLines
+      </v-btn>
+    </v-flex>
+  </v-layout>
+</v-container>
 </template>
 
 <script>
@@ -22,6 +31,11 @@
           { imageUrl: 'http://www.eloriente.net/home/wp-content/uploads/2015/05/Biblioteca-Campus-Monterrey.jpeg', id: '2' },
           { imageUrl: 'http://www.itesm.mx/wps/wcm/connect/899412804385fc01918ad5c0f570d531/rectoria.JPG?MOD=AJPERES&CACHEID=899412804385fc01918ad5c0f570d531', id: '3' }
         ]
+      }
+    },
+    methods: {
+      onClickPolys () {
+        this.$store.dispatch('uploadPoly')
       }
     }
   }
--- Vue/src/store/index.js
+++ Vue/src/store/index.js
@@ -24,6 +24,9 @@ export const store = new Vuex.Store({
     }
   },
   actions: {
+    uploadPoly () {
+      firebase.database().ref('polylines').push({id: '3', line: 'e_h{Cfp~bRqHdDeEdC_FdCsHtBmGfA}CRw]Y}ESaEA}BTiBf@eAj@oAxAiHfKyDzDiF|D_Ah@_AXmE~Bef@tWqp@~]w[dQ}GhDaS`LiFhC}@Ho@|@qP|IqNzHaGnDiBp@yARg@Ng@f@i@hA{@r@}CbBEv@Qz@c@jEaAlIo@`Hg@vDc@xD`G~@bCRxBbDS`@eA~I[hEs@vG_@lEa@tDi@xDiArMcDjZsFc@qHgAqAOwB@iDRySsBqFw@a[{BuKm@mATmUiBeMu@aPoAsCImv@T{NYuFB_AMwBa@}Cy@sDw@eLwCiHyB_X}G{E}AkP_EyVqGa^_JuZ_I_O_DmAm@aBkAqAu@}IkAqH}Am~@_VeVeG__@uJa]mIyMyDuJmDeMmEs{@gZ{MyEuUeIWhCcBpS_AhOm@vKaAdNSdEQx@k@|@cDrC{KdJkU|R{yAhnAeL`J_BhAeA~@[b@Q`A}Ehq@uA`Qi@vIc@|F?bAJh@d@h@j@Pv@?fFu@j^}EbAEdJRdBPrENiA|Q_ABEf@~@^Et@]nDj@b@xACvCeAxGoCtMwFrAcAf@q@f@eAZaBXcCs@KoPe@sLU{IWoAQmIQoAFie@rGs@@g@Qa@e@Mi@?{@^}EHWPGfDk@NKFc@l@qKr@kKZcGlEwo@LSta@e]fs@_l@~\\cZzXsUhAeA`@s@\\_Bh@uKvE}r@ZuFNC~~@|[dJ|CvAR|K`EjCzBtA~@`Cp@pEfAvl@pOdp@dQjGzAfB^nX~Gtc@bL~OfDhGjAfAH~@?rAc@n@m@n@yAZ}@x@uCZs@^g@v@u@|@k@xAq@j@SRDnOlO`ArAb@z@hBlGVl@RLd^vB~k@~DjXdBl[zBfl@tDHRNbc@Dp@^V|UKhUD|@Br@RhDzAdAZfC`@d\\xBfU`BtEBl|@fG`EPjPlB~@H^ORWJs@JoBC_B@cAnB}Qf@uFhBiO\\eB\\mCBmCLaDIcG[yCU_Bh@kGdDq[xBwRJUNOjJeFxFuCbRaKbFeClDoBh@Ul@@`@_@A{@y@_@c@AuIeMA]hJsKfD}EHYIk@gFsHg@i@cDeA?Q~BkOVcCKcB_@aC}@{Ga@kDLQzCoBjH_GpA{@fA_@fAIvH@|FZbAErAS`IwA|Co@vCu@pSiDfEYvRo@bLc@xIHhINpHd@bBZxDjAlDpArGpCx@`@t@Nv@An@_@bBkBxJaNd@[z@a@jA_@zAOfAAzDDzEThBBh@PrBBrACf@SbONfCGfDWzGoAzC_AfHqC~GwC|BgANOJQzBiAJe@c@OY@UN'})
+    },
     signUpUser ({commit}, payload) {
       firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password).then(
         user => {
